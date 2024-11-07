e9cafe9286bddea36c0c5aaeb5aa55a47a4ec004
Danster2020 <daniel.bleckert@me.com>
2024-11-07 23:49:23 +0100

    dark spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const Spinner = () => {
     return (
-        <div className='relative animate-spin h-32 w-32 border-blue-100 border-[1rem] rounded-full'>
+        <div className='relative animate-spin h-32 w-32 border-blue-100 dark:border-def_blue_1 border-[1rem] rounded-full'>
             <div className='absolute left-0 right-0 w-4 h-4 bg-blue-500 rounded-full'></div>
         </div>
     )
